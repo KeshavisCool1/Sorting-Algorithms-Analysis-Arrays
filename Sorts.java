b205ab8aaf9ab5e0eed9e91a488db2abf4989ca2
Shannon Sos <ssos@pleasantonusd.net>
2026-01-26 09:20:04 -0800

    Remove static in merge method sorts.java

--- Sorts.java
+++ Sorts.java
@@ -87,7 +87,7 @@ public class Sorts{
         merge(list, l, r, mid, n - mid);
     }
 
-    public static void merge(int[] a, int[] l, int[] r, int left, int right) {
+    public void merge(int[] a, int[] l, int[] r, int left, int right) {
       int i = 0, j = 0, k = 0;
       while (i < left && j < right) {
         if (l[i] <= r[j])
